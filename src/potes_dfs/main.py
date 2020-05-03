f046b6d8778b57c271d2e41fe11846d3738949fc
Ana <anagmendesp@gmail.com>
2020-05-03 16:50:59 -0400

    Adição de tempo de execução + potes: a saga

--- src/potes_dfs/main.py
+++ src/potes_dfs/main.py
@@ -1,26 +1,13 @@
-import src.missionarios_dfs.dfs
+import src.potes_dfs.dfs
 
 from datetime import datetime
 
-
-movimentos_validos = []
-mov1 = src.missionarios_dfs.dfs.Movimento(1, 0)
-mov2 = src.missionarios_dfs.dfs.Movimento(2, 0)
-mov3 = src.missionarios_dfs.dfs.Movimento(0, 1)
-mov4 = src.missionarios_dfs.dfs.Movimento(0, 2)
-mov5 = src.missionarios_dfs.dfs.Movimento(1, 1)
-
-movimentos_validos.append(mov1)
-movimentos_validos.append(mov2)
-movimentos_validos.append(mov3)
-movimentos_validos.append(mov4)
-movimentos_validos.append(mov5)
-
-no_inicial = src.missionarios_dfs.dfs.No(3, 3, 1)
-no_final = src.missionarios_dfs.dfs.No(0, 0, 0)
-grafo = src.missionarios_dfs.dfs.Grafo(no_inicial, no_final, movimentos_validos)
+pote5 = src.potes_dfs.dfs.Pote(0,5)
+pote7 = src.potes_dfs.dfs.Pote(0,7)
+no_inicial = src.potes_dfs.dfs.No(pote5,pote7)
+grafo = src.potes_dfs.dfs.Grafo(no_inicial)
 grafo.gerar_grafo()
-dfs = src.missionarios_dfs.dfs.DFS(grafo.get_lista_nos_grafo(), no_inicial, no_final)
+dfs = src.potes_dfs.dfs.DFS(grafo.get_lista_nos_grafo(), no_inicial)
 
 inicio = datetime.now()
 dfs.busca_em_profundidade()
